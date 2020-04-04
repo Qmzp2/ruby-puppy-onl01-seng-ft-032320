9480b15c66692dfa1b7ed3e4fe6a6f15f8d2118b
Kevin Mathias <komathias1@gmail.com>
2020-04-04 09:11:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
-# Add your code here
+
+
+class Dog
+  attr_accessor :name
+  @@all = []
+  @@name = []
+  
+  
+  def initialize (name)
+    @name = name
+    @@name << @name
+   @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.print_all
+    puts @@all.name
+  end
+  
+  def self.save
+    @@all << self
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+  
+  
+  
+end
